93b764f7e5f512a4661089d27842f0754fdba99f
Michael <yagodarov.misha.2003@mail.ru>
2022-09-09 00:33:21 +0300

    feat: default hello world

---
+++ hello_world.asm
@@ -0,0 +1,39 @@
+assume ss:my_stack, cs:my_code
+
+my_stack segment
+    stack dw 12 dup('?')
+my_stack ends
+
+my_data segment
+    hello_message db 'Hello world!', 0ah, 0dh, '$'
+my_data ends
+
+my_code segment
+
+display_message proc near
+    push ax
+
+    mov ah, 09h
+    int 21h
+
+    pop ax
+    ret
+display_message endp
+
+main proc far
+    push ds
+    sub ax, ax
+    push ax
+
+    mov ax, my_data
+    mov ds, ax
+
+    mov dx, offset hello_message
+    call display_message
+
+    ret
+main endp
+
+my_code ends
+
+end main
