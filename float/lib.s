aa8703c8646f2a84ee46bdb56b7d2ee7f1e71a84
Purposelessness <purposelessness@yandex.ru>
2022-12-02 06:11:47 +0300

    fix: count == 0

--- float/lib.s
+++ float/lib.s
@@ -7,9 +7,12 @@
 poly:
   movsd xmm1, xmm0
   subsd xmm0, xmm0
+  test  rdi, rdi
+  jz    poly_end
   mov   rcx, rdi
- horner:
+horner:
    mulsd xmm0, xmm1
    addsd xmm0, [rsi + rcx * 8 - 8]
    loop  horner
+poly_end:
   ret
