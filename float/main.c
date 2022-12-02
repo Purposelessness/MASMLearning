813d36020dff61cb5ac652b3e41d322e849e736b
Purposelessness <purposelessness@yandex.ru>
2022-12-02 05:39:38 +0300

    feat: time checks

--- float/main.c
+++ float/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 extern double poly(double x, int n, double *c);
 
@@ -20,14 +21,21 @@ int main() {
   }
 
   double test = 0;
+  clock_t c_time = clock();
   for (int i = n - 1; i > -1; --i) {
     test *= x;
     test += constants[i];
   }
+  c_time = clock() - c_time;
 
+  clock_t asm_time = clock();
   double result = poly(x, n, constants);
-  printf("Result is:\n%lf\n", result);
-  printf("Test is:\n%lf\n", test);
+  asm_time = clock() - asm_time;
+
+  printf("(asm) Result is:\n\t%lf (%ld : %lf)\n", result, asm_time,
+         (double)asm_time / CLOCKS_PER_SEC);
+  printf("(c) Test is:\n\t%lf (%ld : %lf)\n", test, c_time,
+         (double)c_time / CLOCKS_PER_SEC);
 
   free(constants);
 
